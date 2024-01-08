f23b8320e304a21b1c35cbf8bf3db30ca647ebce
kevin-llps <llopis.kevin@outlook.fr>
2024-01-08 23:34:12 +0100

    feat: Remove mapper and reformat code

--- src/main/java/fr/kevin/llps/conf/event/reminder/service/TalkService.java
+++ src/main/java/fr/kevin/llps/conf/event/reminder/service/TalkService.java
@@ -1,36 +1,54 @@
 package fr.kevin.llps.conf.event.reminder.service;
 
+import fr.kevin.llps.conf.event.reminder.domain.Speaker;
 import fr.kevin.llps.conf.event.reminder.domain.Talk;
 import fr.kevin.llps.conf.event.reminder.service.database.MySQLDatabaseService;
-import fr.kevin.llps.conf.event.reminder.service.database.mapper.TalkMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class TalkService {
 
     private static final String SELECT_TALKS = "SELECT t.date, t.title, " +
             "t.description, s.firstname, s.lastname " +
             "FROM talk t JOIN speaker s " +
             "ON t.speaker_id = s.speaker_id " +
-            "ORDER BY t.date,t.title;";
+            "ORDER BY t.date;";
 
-    private final MySQLDatabaseService databaseService;
-    private final TalkMapper talkMapper;
+    private final MySQLDatabaseService mySQLDatabaseService;
+
+    public TalkService(MySQLDatabaseService mySQLDatabaseService) {
+        this.mySQLDatabaseService = mySQLDatabaseService;
+    }
 
     public List<Talk> getTalks() {
-        try (Connection connection = databaseService.openConnection();
+        try (Connection connection = mySQLDatabaseService.openConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_TALKS);
              ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            return talkMapper.mapToDomainList(resultSet);
+            List<Talk> talks = new ArrayList<>();
+
+            while (resultSet.next()) {
+                LocalDateTime date = LocalDateTime.parse(resultSet.getString(1), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                String title = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                String speakerFirstname = resultSet.getString(4);
+                String speakerLastname = resultSet.getString(5);
+
+                Speaker speaker = new Speaker(speakerFirstname, speakerLastname);
+
+                talks.add(new Talk(title, description, date, speaker));
+            }
+
+            return talks;
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/test/java/fr/kevin/llps/conf/event/reminder/service/TalkServiceTest.java
+++ src/test/java/fr/kevin/llps/conf/event/reminder/service/TalkServiceTest.java
@@ -2,7 +2,6 @@ package fr.kevin.llps.conf.event.reminder.service;
 
 import fr.kevin.llps.conf.event.reminder.domain.Talk;
 import fr.kevin.llps.conf.event.reminder.service.database.MySQLDatabaseService;
-import fr.kevin.llps.conf.event.reminder.service.database.mapper.TalkMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,10 +12,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static fr.kevin.llps.conf.event.reminder.samples.TalkSample.talkList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,16 +25,11 @@ class TalkServiceTest {
     @Mock
     private MySQLDatabaseService databaseService;
 
-    @Mock
-    private TalkMapper talkMapper;
-
     @InjectMocks
     private TalkService talkService;
 
     @Test
     void shouldGetTalks() throws SQLException {
-        List<Talk> expectedTalks = talkList();
-
         Connection connection = mock(Connection.class);
         PreparedStatement preparedStatement = mock(PreparedStatement.class);
         ResultSet resultSet = mock(ResultSet.class);
@@ -42,11 +37,27 @@ class TalkServiceTest {
         when(databaseService.openConnection()).thenReturn(connection);
         when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        when(talkMapper.mapToDomainList(resultSet)).thenReturn(expectedTalks);
+        when(resultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(resultSet.getString(1)).thenReturn("2023-02-05 19:00:00").thenReturn("2023-06-28 19:00:00");
+        when(resultSet.getString(2)).thenReturn("conf1").thenReturn("conf2");
+        when(resultSet.getString(3)).thenReturn("Description conf1").thenReturn("Description conf2");
+        when(resultSet.getString(4)).thenReturn("jean").thenReturn("alice");
+        when(resultSet.getString(5)).thenReturn("dupont").thenReturn("dupont");
 
         List<Talk> talks = talkService.getTalks();
 
-        assertThat(talks).containsExactlyInAnyOrderElementsOf(expectedTalks);
+        assertThat(talks).isNotNull()
+                .hasSize(2)
+                .extracting("title", "description", "date", "speaker.firstname", "speaker.lastname")
+                .containsExactlyInAnyOrder(
+                        tuple("conf1",
+                                "Description conf1",
+                                LocalDateTime.of(2023, 2, 5, 19, 0, 0),
+                                "jean", "dupont"),
+                        tuple("conf2",
+                                "Description conf2",
+                                LocalDateTime.of(2023, 6, 28, 19, 0, 0),
+                                "alice", "dupont"));
 
         verifyNoMoreInteractions(databaseService);
     }
